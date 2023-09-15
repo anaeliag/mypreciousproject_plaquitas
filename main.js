08e295c2c20f4fffe479aba70917224d50ca3efd
Ana Garcia <sailor_ana@hotmail.com>
2023-09-14 20:58:36 -0600

    adding changes to logic

--- main.js
+++ main.js
@@ -46,12 +46,15 @@ function login() {
         let quierescuenta = prompt("Quieres crear tu cuenta? (Responde si o no)");
         if (quierescuenta === "si") {
             const nombre = prompt("cual es tu nombre? ");
-            const usuario = prompt("cual es tu usuario? ");
+            usuario = prompt("cual es tu usuario? ");
             const contrasena = prompt("cual es tu contrasena? ");
             console.log("Bienvenido "+usuario)
             const newUser = new Usuario(usuario, nombre, contrasena);
             const usermasnewuser = BD.concat(newUser);
             console.log(usermasnewuser)
+            alert(document.getElementById("areaMensajes").innerText = "Bienvenido "+usuario);
+            console.log("Bienvenida/o "+usuario+", estaremos contentos de servirte");
+            document.getElementById("lascompras").addEventListener("click", compradeusuariologueado);
         } else {
             alert("Puedes navegar sin tener cuenta, solo no podras comprar :(")
         }
@@ -194,12 +197,12 @@ function compradeusuariologueado(){
                     } else if (Number(plaquita) == 6) {
                         return
                     } else {
-                        console.log("Esa opcion no es valida, por favor vuelve a loguearte");
-                        login();
+                        console.log("Esa opcion no es valida, por favor vuelve a intentarlo");
+                        return;
                     }
             } else {
-                    console.log("Ok, puedes elegir mas productos");
-                    login();
+                    console.log("Ok, puedes elegir tus productos despues");
+                    return;
             }
             let nombredeperrito = prompt("Cual es en nombre de tu peludito?");
             console.log(typeof nombredeperrito)
@@ -215,8 +218,9 @@ function compradeusuariologueado(){
                 console.log("Numero de telefono es valido: "+numerodetelefono);
                 //return true;
             } else {
+                alert("Numero invalido, vuelve a intentarlo")
                 console.log("Numero invalido");
-                compra()
+                return;
             }
         //}//??
             const colordeplaquita = prompt(`Que color de plaquita quieres elegir (ejemplo: 1, 2 3 etc)));
@@ -256,7 +260,7 @@ function compradeusuariologueado(){
                     console.log("Elegiste: "+pago+" - Paypal");
                     let correo = prompt("Ingresa tu correo electronico)");
                     if (correo.includes("@hotmail.com") || correo.includes("@gmail.com")) {
-                        let direccion = prompt("Ingresa tu direccion (Avenida siempre viva #45, Springfield)");
+                        let direccion = prompt("Ingresa tu direccion (Ejemplo; Avenida siempre viva #45, Springfield)");
                         if (direccion != null) {
                             alert("Tu pago a sido aplicado a tu cuenta: "+correo+" y te enviaremos tu plaquita a la direccion proporcionada, gracias por tu compra!");
                             return;
@@ -265,34 +269,30 @@ function compradeusuariologueado(){
                             return;
                         }
                     } else {
-                        alert("Correo no valido, vuelve a inetntarlo");
+                        alert("Correo no valido, vuelve a intentarlo");
                         return;
                     }
                 } else if (Number(pago) == 2) {
                     console.log("Elegiste: "+pago+" - Tarjeta de Debito o Credito");
                     let cuenta = prompt("Ingresa tu numero de tarjeta del siguiente modo (xxxx-xxxx-xxxx-xxxx)");
                     if (validartarjeta(cuenta)) {
-                        let direccion = prompt("Ingresa tu direccion (Avenida siempre viva #45, Springfield)");
+                        let direccion = prompt("Ingresa tu direccion (Ejemplo; Avenida siempre viva #45, Springfield)");
                         if (direccion != null) {
                             console.log("Numero de tarjeta valido: "+cuenta);
                             alert("Tu pago a sido aplicado a tu cuenta y te enviaremos tu plaquita a la direccion proporcionada, gracias por tu compra!");
                             return;
                         } else {
-                            alert("Direccion invalida, vuelve a intentarlo");
-                            return;
-                        //return true;
-                    } else {
                         console.log("Tarjeta invalida, vuelve a intentarlo");
-                        compra()
+                        return;
+                        }
                     }
-                } else if (Number(pago) == 3) {
+                }  else if (Number(pago) == 3) {
                     return;
                 } else {
                     console.log("Esa opcion no es valida, por favor vuelve a intentarlo");
-                login();
+                    return;
                 }
-                }
-            /*console.log(BD[i].productos.push([plaquita,nombredeperrito,numerodetelefono,colordeplaquita]));
+            }    /*console.log(BD[i].productos.push([plaquita,nombredeperrito,numerodetelefono,colordeplaquita]));
             console.log(productoyuser);*///close if user    //close for 
 }//close function
 
@@ -306,12 +306,3 @@ document.body.onload = login()
 } else {
     document.getElementById("lascompras").addEventListener("click", compra); //nodo
 }*/
-
-
-
-
-
-
-
-
-
